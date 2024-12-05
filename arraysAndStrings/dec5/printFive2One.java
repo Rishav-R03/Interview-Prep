fd58294c0c58fec1411396fa4fa888517a7c9f85
Rishav-R03 <rishav042023@gmail.com>
2024-12-05 09:57:05 +0530

    print 5 to 1

--- arraysAndStrings/dec5/printFive2One.java
+++ arraysAndStrings/dec5/printFive2One.java
@@ -4,11 +4,13 @@ public class printFive2One {
         if (n == 0) {
             return;
         }
+        // recursive case
         System.out.println(n);
         printNum(n - 1);
     }
 
     public static void main(String[] args) {
+        //function call
         printNum(5);
     }
 }
