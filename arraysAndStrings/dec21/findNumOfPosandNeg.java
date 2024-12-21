3ab82b60b9494887b987d5cdd8383eee3e88bd60
Rishav-R03 <rishav042023@gmail.com>
2024-12-21 10:46:35 +0530

    find the max of pos elements and neg elements

---
+++ arraysAndStrings/dec21/findNumOfPosandNeg.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class findNumOfPosandNeg {
+    public int maximumCount(int[] nums) {
+        int n = nums.length;
+        int pos = 0;
+        int neg = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < 0) {
+                neg++;
+            } else if (nums[i] > 0) {
+                pos++;
+            }
+        }
+        return Math.max(pos, neg);
+    }
+    
+    public static void main(String[] args) {
+        findNumOfPosandNeg obj = new findNumOfPosandNeg();
+        int[] nums = { -1, -2, 3, 4, 5 };
+        int result = obj.maximumCount(nums);
+        System.out.println(result);
+    }
+}
