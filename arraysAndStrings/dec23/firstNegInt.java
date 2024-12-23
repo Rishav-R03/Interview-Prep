3802dac9201e35beaab4a8a9e155355d1a948a77
Rishav-R03 <rishav042023@gmail.com>
2024-12-23 17:45:44 +0530

    first neg element

---
+++ arraysAndStrings/dec23/firstNegInt.java
@@ -0,0 +1,9 @@
+public class firstNegInt {
+    static int firstNegative(int[] arr, int k) {
+        int n = arr.length;
+        if (n < k) {
+            return -1;
+        }
+        
+    }
+}
