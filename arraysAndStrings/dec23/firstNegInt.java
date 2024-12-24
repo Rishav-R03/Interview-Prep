8ca407841e2268919b8f21d04077777eb19eedd5
Rishav-R03 <rishav042023@gmail.com>
2024-12-25 00:13:31 +0530

    first neg element

--- arraysAndStrings/dec23/firstNegInt.java
+++ arraysAndStrings/dec23/firstNegInt.java
@@ -4,12 +4,20 @@ public class firstNegInt {
         if (n < k) {
             return -1;
         }
-
         int windowSum = 0;
         for (int i = 0; i < k; i++) {
             windowSum += arr[i];
         }
-        
+        if (windowSum > 0) {
+            return -1;
+        }
+        int res = 0;
+        for (int i = k; i < n; i++) {
+            windowSum += arr[i];
+            if (windowSum > 0) {
+                return i - k + 1;
+            }
+        }
         return 1;
     }
 }
